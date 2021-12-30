31b1593fbbf749dc3ac6db72d1b767f6d3f48e95
Thanh Do <dothanhqt9x@gmail.com>
2021-12-30 14:23:29 +0700

    commit model

---
+++ src/main/java/model/entity/RoleUserEntity.java
@@ -0,0 +1,18 @@
+package model.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Table(name = "ROLE_USER")
+@Entity
+public class RoleUserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private int userId;
+
+    private int roleId;
+}
---
+++ src/main/java/model/entity/UserEntity.java
@@ -0,0 +1,21 @@
+package model.entity;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Data
+@Table(name = "USER")
+@Entity
+public class UserEntity extends BaseEntity{
+    private String username;
+
+    private String password;
+
+    private String fullName;
+
+    private String address;
+
+    private String email;
+}
